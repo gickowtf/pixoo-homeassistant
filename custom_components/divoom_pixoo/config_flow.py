e50133bf9cbf327f1b5934ce5d90c9eed1fd755c
Mrredstone5230 <45701824+Mrredstone5230@users.noreply.github.com>
2024-02-22 23:05:35 -0500

    Moved the configuration to the config flow (So the integration is now using the UI instead of the config file)

---
+++ custom_components/divoom_pixoo/config_flow.py
@@ -0,0 +1,75 @@
+from homeassistant import config_entries
+from homeassistant.config_entries import ConfigEntry, OptionsFlow
+from homeassistant.helpers.selector import ObjectSelector, ObjectSelectorConfig, TextSelector, TextSelectorConfig, \
+    DurationSelector, DurationSelectorConfig, NumberSelector, NumberSelectorConfig, NumberSelectorMode
+
+from .const import DOMAIN, CURRENT_ENTRY_VERSION
+import voluptuous as vol
+from . import load_pixoo
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ConfigFlowHandler(config_entries.ConfigFlow, config_entries.OptionsFlow, domain=DOMAIN):
+    VERSION = CURRENT_ENTRY_VERSION  # Version for new entries. Managed by HA
+
+    def __init__(self, config_entry: ConfigEntry = None):
+        self.entry_options = config_entry.options if config_entry else {"ip_address": "", "scan_interval": "15",
+                                                                        "pages_data": [{'page': 1, 'PV': [{'power': '{{ states.sensor.YOUR_SENSOR.state }}', 'storage': '{{ states.sensor.YOUR_SENSOR.state }}', 'discharge': '{{ states.sensor.YOUR_SENSOR.state }}', 'powerhousetotal': '{{ states.sensor.YOUR_SENSOR.state }}', 'vomNetz': '{{ states.sensor.YOUR_SENSOR.state }}', 'time': "{{ now().strftime('%H:%M') }}"}]}, {'page': 2, 'texts': [{'text': 'github/gickowtf', 'position': [0, 10], 'font': 'FONT_PICO_8', 'font_color': [255, 0, 0]}, {'text': 'Thx 4 Support', 'position': [0, 30], 'font': 'FONT_PICO_8', 'font_color': [255, 0, 0]}], 'images': [{'image': '/config/custom_components/divoom_pixoo/img/haus.png', 'position': [30, 10]}]}, {'page': 3, 'channel': [{'number': 2}]}, {'page': 4, 'clockId': [{'number': 39}]}]}
+
+    async def async_step_user(self, user_input: dict = None):
+        errors = {}
+        if user_input is not None:
+            try:
+                pix = await self.hass.async_add_executor_job(load_pixoo, user_input.get('ip_address'))
+                if await self.verify_unique_device(user_input.get('ip_address')):
+                    # TODO: Add config verification
+                    return self.async_create_entry(title="Divoom Pixoo 64", data=user_input,
+                                                   options=user_input)  # it's required to set
+                # data anyways since otherwise the option flow won't save the data. (HA bug?) PS: DATA ISN'T USED HERE.
+                else:
+                    errors["ip_address"] = "already_configured"
+            except Exception as e:
+                _LOGGER.error("Error setting up Pixoo: %s", e)
+                errors["ip_address"] = "connection"
+        else:
+            user_input = {}
+
+        return self.async_show_form(
+            step_id="user", errors=errors, data_schema=vol.Schema({
+                vol.Required("ip_address",
+                             default=user_input.get("ip_address", self.entry_options.get("ip_address"))): str,
+                vol.Required("scan_interval", default=user_input.get("scan_interval", self.entry_options.get(
+                    "scan_interval"))): NumberSelector(
+                    NumberSelectorConfig(min=1, max=9999, step=1, mode=NumberSelectorMode.BOX, unit_of_measurement="seconds")
+                ),
+                vol.Required("pages_data",
+                             default=user_input.get("pages_data",
+                                                    self.entry_options.get("pages_data"))): ObjectSelector(
+                    ObjectSelectorConfig()
+
+                )
+            })
+        )
+
+    # Gives the OptionsFlow class to HA
+    def async_get_options_flow(config_entry):
+        return ConfigFlowHandler(config_entry)
+
+    async def async_step_init(self, user_input=None):
+        # This is set since the Option Flow's first step is always init.
+        return await self.async_step_user(user_input)
+
+    async def verify_unique_device(self, ip_address: str):
+        """Check if the device is already configured."""
+
+        # return True #DEBUG
+        if ip_address == self.entry_options.get("ip_address"):  # If the user didn't change the IP, it's still valid.
+            return True
+
+        for entry in self.hass.config_entries.async_entries(DOMAIN):
+            if entry.options['ip_address'] == ip_address:
+                return False
+        return True
--- custom_components/divoom_pixoo/light.py
+++ custom_components/divoom_pixoo/light.py
@@ -1,11 +1,6 @@
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
-from homeassistant.components.light import (ATTR_BRIGHTNESS, PLATFORM_SCHEMA, LightEntity)
-from homeassistant.const import CONF_IP_ADDRESS
-from homeassistant.core import HomeAssistant
+from homeassistant.components.light import (LightEntity)
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from . import DOMAIN, VERSION
 
 from .pixoo64._pixoo import Pixoo
@@ -13,42 +8,29 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_IP_ADDRESS): cv.string,
-    })
 
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
-    """Set up the Awesome Light platform."""
-    if discovery_info is None:
-        _LOGGER.error("No discovery_info found in Divoom Pixoo light setup")
-        return
-    ip_address = discovery_info.get(CONF_IP_ADDRESS)
-    if ip_address is None:
-        _LOGGER.error("No IP address found for Divoom Pixoo light")
-        return
-
-    light = DivoomLight(ip_address)
-    add_entities([light], True)
+async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities):
+    async_add_entities([ DivoomLight(config_entry=config_entry, pixoo=hass.data[DOMAIN][config_entry.entry_id]["pixoo"]) ], True)
 
 
 class DivoomLight(LightEntity):
-    def __init__(self, ip_address):
+    def __init__(self, ip_address=None, config_entry: ConfigEntry = None, pixoo: Pixoo = None):
         self._ip_address = ip_address
-        self._name = "Divoom Pixoo 64 Light"
-        self._state = None
+        self._config_entry = config_entry
+        self._attr_has_entity_name = True
+        self._name = "Light"
+        self._pixoo = Pixoo(self._ip_address) if pixoo is None else pixoo
         self._brightness = None
-        self._pixoo = Pixoo(self._ip_address)
+        self._state = None
         _LOGGER.debug(f"Divoom Pixoo IP address from configuration: {self._ip_address}")
 
     @property
     def name(self) -> str:
         """Return the display name of this light."""
         return self._name
+
+    def update(self):
+        self._state = self._pixoo.get_state()
 
     @property
     def brightness(self):
@@ -58,7 +40,6 @@ class DivoomLight(LightEntity):
     def is_on(self) -> bool | None:
         return self._state
 
-
     def turn_on(self, **kwargs):
         self._state = True
         self._pixoo.set_screen(True)
@@ -67,23 +48,15 @@ class DivoomLight(LightEntity):
         self._state = False
         self._pixoo.set_screen(False)
 
-
-    def update(self) -> None:
-        self._state = self._pixoo.get_state()
-
-    @property
-    def name(self):
-        return self._name
-
     @property
     def unique_id(self):
-        return f"divoom_pixoo_light"
+        return "light_" + str(self._config_entry.entry_id)
 
     @property
     def device_info(self) -> DeviceInfo:
         return DeviceInfo(
-            identifiers={(DOMAIN, self.unique_id)},
-            name=self.name,
+            identifiers={(DOMAIN, str(self._config_entry.entry_id)) if self._config_entry is not None else (DOMAIN, "divoom")},
+            name=self._config_entry.title,
             manufacturer="Divoom",
             model="Pixoo",
             sw_version=VERSION,
